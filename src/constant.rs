197959a73b2ab4e0cf23f70059e246e8bbf47505
Elliot Bentine <elliot_bentine@hotmail.com>
2019-08-09 16:47:29 +0100

    Use rust mathematical constants

--- src/constant.rs
+++ src/constant.rs
@@ -6,10 +6,10 @@ pub const HBAR: f64 = 1.0545718e-34;
 pub const GC: f64 = 9.80665;
 
 /// Mathematical constant exp(1)
-pub const EXP: f64 = 2.718281828;
+pub const EXP: f64 = std::f64::consts::E;
 
 /// Mathematica constant pi
-pub const PI: f64 = 3.1415926535;
+pub const PI: f64 = std::f64::consts::PI;
 
 // The Bohr magneton, defined in SI units of Joules/Tesla.
 pub const BOHRMAG: f64 = 9.274e-24;
