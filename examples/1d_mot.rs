4bb03081ee8d497bb32a08e4cad8fcd252396a34
Elliot Bentine <elliot_bentine@hotmail.com>
2019-09-03 22:16:29 +0100

    Added an example of a 1D MOT - but the behavior is not as expected.

---
+++ examples/1d_mot.rs
@@ -0,0 +1,96 @@
+//! Simulate a 1D MOT.
+//! 
+//! The 1D MOT is formed by counter-propagating laser beams along the z-axis.
+
+extern crate magneto_optical_trap as lib;
+extern crate nalgebra;
+use lib::atom::{Atom, AtomInfo, Force, Mass, Position, Velocity};
+use lib::ecs;
+use lib::initiate::NewlyCreated;
+use lib::integrator::Timestep;
+use lib::laser::cooling::CoolingLight;
+use lib::laser::gaussian::GaussianBeam;
+use lib::magnetic::quadrupole::QuadrupoleField3D;
+use lib::output::file_output::FileOutputSystem;
+use nalgebra::Vector3;
+use specs::{Builder, World};
+
+fn main() {
+    let mut world = World::new();
+    ecs::register_components(&mut world);
+    ecs::register_resources(&mut world);
+    let mut builder = ecs::create_simulation_dispatcher_builder();
+
+    // Add some output to the simulation
+    builder = builder.with(
+        FileOutputSystem::<Position>::new("pos.txt".to_string(), 20),
+        "",
+        &[],
+    );
+    builder = builder.with(
+        FileOutputSystem::<Velocity>::new("vel.txt".to_string(), 20),
+        "",
+        &[],
+    );
+
+    let mut dispatcher = builder.build();
+    dispatcher.setup(&mut world.res);
+
+    // Create magnetic field.
+    world
+        .create_entity()
+        .with(QuadrupoleField3D::gauss_per_cm(15.0))
+        .with(Position::new())
+        .build();
+
+    // Create cooling lasers.
+    let detuning = -6.0;
+    let power = 100.0;
+    world
+        .create_entity()
+        .with(GaussianBeam {
+            intersection: Vector3::new(0.0, 0.0, 0.0),
+            e_radius: 0.01,
+            power: power,
+            direction: -Vector3::z(),
+        })
+        .with(CoolingLight::for_species(AtomInfo::rubidium(), detuning, -1.0))
+        .build();
+    world
+        .create_entity()
+        .with(GaussianBeam {
+            intersection: Vector3::new(0.0, 0.0, 0.0),
+            e_radius: 0.01,
+            power: power,
+            direction: Vector3::z(),
+        })
+        .with(CoolingLight::for_species(AtomInfo::rubidium(), detuning, -1.0))
+        .build();
+
+    // Create atoms
+    for i in 0..20 {
+        world
+            .create_entity()
+            .with(Position {
+                pos: Vector3::new(0.0, 0.0, -0.03),
+            })
+            .with(Atom)
+            .with(Force::new())
+            .with(Velocity {
+                vel: Vector3::new(0.0, 0.0, 5.0 + (i as f64) * 1.0),
+            })
+            .with(NewlyCreated)
+            .with(AtomInfo::rubidium())
+            .with(Mass { value: 87.0 })
+            .build();
+    }
+
+    // Define timestep
+    world.add_resource(Timestep { delta: 1.0e-6 });
+
+    // Run the simulation for a number of steps.
+    for _i in 0..50000 {
+        dispatcher.dispatch(&mut world.res);
+        world.maintain();
+    }
+}
