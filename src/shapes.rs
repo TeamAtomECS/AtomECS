e137a6638cfbd64bf5d8d9d69461731edf5c7fcb
Elliot Bentine <elliot_bentine@hotmail.com>
2020-05-22 17:33:15 +0100

    Added sphere and cuboid shapes. Not yet tested.

--- src/shapes.rs
+++ src/shapes.rs
@@ -1,6 +1,7 @@
 //! Support for different shapes.
 
 use nalgebra::Vector3;
+use specs::{Component,HashMapStorage}
 extern crate rand;
 use rand::Rng;
 
@@ -25,6 +26,10 @@ pub struct Cylinder {
     pub perp_x: Vector3<f64>,
     /// A normalised vector, aligned perpendicular to the cylinder.
     pub perp_y: Vector3<f64>,
+}
+
+impl Component for Cylinder {
+    type Storage = HashMapStorage<Self>;
 }
 
 impl Volume for Cylinder {
@@ -67,4 +72,91 @@ impl Surface for Cylinder {
             return (point, normal);
         }
     }
+}
+
+
+/// A sphere.
+pub struct Sphere {
+    pub radius: f64,
+}
+
+impl Volume for Sphere {
+    fn contains(&self, volume_position: &Vector3<f64>, entity_position: &Vector3<f64>) -> bool {
+        let delta = entity_position - volume_position;
+        return delta.norm_squared() < self.radius.powi(2);
+    }
+}
+
+impl Surface for Sphere {
+    fn get_random_point_on_surface(&self, surface_position: &Vector3<f64>) -> (Vector3<f64>, Vector3<f64>) {
+        let mut rng = rand::thread_rng();
+
+        let theta = rng.gen_range(0.0, std::f64::consts::PI)
+        let phi = rng.gen_range(0.0, 2*std::f64::consts::PI)
+
+        let normal = Vector3::new(
+            theta.sin() * phi.cos(),
+            theta.sin() * phi.sin(),
+            theta.cos()
+        )
+        let position = surface_position + self.radius * normal;
+        return (position, normal);
+    }
+}
+
+impl Component for Sphere {
+    type Storage = HashMapStorage<Self>;
+}
+
+/// A cuboid.
+pub struct Cuboid {
+    /// The dimension of the cuboid volume, from center to vertex (1,1,1).
+    pub half_width: Vector3<f64>,
+}
+
+impl Volume for Cuboid {
+    fn contains(&self, volume_position: &Vector3<f64>, entity_position: &Vector3<f64>) -> bool {
+        let delta = entity_position - volume_position;
+        return delta[0].abs() < self.half_width[0]
+            && delta[1].abs() < self.half_width[1]
+            && delta[2].abs() < self.half_width[2];
+    }
+}
+
+impl Surface for Cuboid {
+    fn get_random_point_on_surface(&self, surface_position: &Vector3<f64>) -> (Vector3<f64>, Vector3<f64>) {
+        let mut rng = rand::thread_rng();
+
+        let point = Vector3::new(
+            rng.gen_range(-self.half_width[0], self.half_width[0]),
+            rng.gen_range(-self.half_width[1], self.half_width[1]),
+            rng.gen_range(-self.half_width[2], self.half_width[2])
+            );
+
+        // move to a random edge
+        let edge = rng.gen_range(0, 6);
+        match edge {
+            0 => point[0] = -self.half_width[0],
+            1 => point[0] = self.half_width[0],
+            2 => point[1] = -self.half_width[1],
+            3 => point[1] = self.half_width[1],
+            4 => point[2] = -self.half_width[2],
+            5 => point[2] = self.half_width[2],
+        }
+
+        let normal = match edge {
+            0 => Vector3::new(-1.0,0.0,0.0),
+            1 => Vector3::new(1.0,0.0,0.0),
+            2 => Vector3::new(0.0,-1.0,0.0),
+            3 => Vector3::new(0.0,1.0,0.0),
+            4 => Vector3::new(0.0,0.0,-1.0),
+            5 => Vector3::new(0.0,0.0,1.0),
+        }
+        let position = surface_position + point;
+        return (position, normal);
+    }
+}
+
+impl Component for Cuboid {
+    type Storage = HashMapStorage<Self>;
 }
