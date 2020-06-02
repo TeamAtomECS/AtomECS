2713d05a7c572f91d5e2d22df8707d9d96b7356b
Elliot Bentine <elliot_bentine@hotmail.com>
2020-06-02 10:14:57 +0100

    Fixed the warnings in create_magnetic_grid

--- examples/create_magnetic_grid.rs
+++ examples/create_magnetic_grid.rs
@@ -1,23 +1,20 @@
-//! Simulate a 1D MOT.
-//!
-//! The 1D MOT is formed by counter-propagating laser beams along the z-axis.
+//! Create a magnetic grid file. This is really just used to show you what format the program expects the file to be in.
 
 extern crate magneto_optical_trap as lib;
 extern crate nalgebra;
 use lib::magnetic::grid::PrecalculatedMagneticFieldGrid;
 use nalgebra::Vector3;
-use specs::{Builder, World};
 use std::fs::File;
 extern crate serde;
 extern crate serde_json;
 
 fn main() {
     let grid = PrecalculatedMagneticFieldGrid {
-        extent_spatial: Vector3::new(1.0,1.0,1.0),
-        extent_cells: Vector3::new(1,1,1),
-        position: Vector3::new(0.0,0.0,0.0),
-        grid: vec!(Vector3::new(1.0,1.0,1.0))
-        };
-    let mut file = File::create("grid.json").expect("Cant open file");
-    serde_json::to_writer(file, &grid);
+        extent_spatial: Vector3::new(1.0, 1.0, 1.0),
+        extent_cells: Vector3::new(1, 1, 1),
+        position: Vector3::new(0.0, 0.0, 0.0),
+        grid: vec![Vector3::new(1.0, 1.0, 1.0)],
+    };
+    let file = File::create("grid.json").expect("Cant open file");
+    serde_json::to_writer(file, &grid).expect("Could not serialize grid");
 }
