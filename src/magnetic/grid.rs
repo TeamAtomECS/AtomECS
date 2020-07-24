29cdd3f5c44d990d6adfec6ad96fa1f3566e95e5
Elliot Bentine <elliot_bentine@hotmail.com>
2020-07-24 11:50:41 +0100

    Noticed a bug in indices ordering for magnetic field grids.

--- src/magnetic/grid.rs
+++ src/magnetic/grid.rs
@@ -1,26 +1,26 @@
 //! Define magnetic fields using grids.
 
 extern crate nalgebra;
-use nalgebra::Vector3;
 use crate::atom::Position;
 use crate::magnetic::MagneticFieldSampler;
+use nalgebra::Vector3;
 use specs::{Component, HashMapStorage, Join, ReadStorage, System, WriteStorage};
 extern crate serde;
 use serde::{Deserialize, Serialize};
 
 /// Defines a magnetic field using a grid-based representation.
-/// 
+///
 /// The grid is ordered as a linear array, with elements ordered in priority z,y,x;
 /// items with dz=1 are adjacent in memory.
-/// 
+///
 /// # Fields
-/// 
+///
 /// `extent_spatial`: Size of the grid, in units of m.
-/// 
+///
 /// `position`: Position of the grid center, in units of m.
-/// 
+///
 /// `extent_cells`: Size of the grid in cells, along the (x,y,z) axes.
-/// 
+///
 /// `grid`: `Vec<Vector3<f64>>` containing the field at each grid cell.
 #[derive(Serialize, Deserialize)]
 pub struct PrecalculatedMagneticFieldGrid {
@@ -32,15 +32,22 @@ pub struct PrecalculatedMagneticFieldGrid {
 
 impl PrecalculatedMagneticFieldGrid {
     pub fn position_to_grid_index(&self, pos: &Vector3<f64>) -> i32 {
-        let delta = pos - self.position + self.extent_spatial / 2.0;
+        let delta = pos - (self.position - self.extent_spatial / 2.0);
         let fraction = delta.component_div(&self.extent_spatial);
+        // calculate cell ids
         let cell_id = Vector3::new(
-            (fraction[0].max(0.0).min(1.0) * self.extent_cells[0] as f64) as i32,
-            (fraction[1].max(0.0).min(1.0) * self.extent_cells[1] as f64) as i32,
-            (fraction[2].max(0.0).min(1.0) * self.extent_cells[2] as f64) as i32,
+            ((fraction[0] * self.extent_cells[0] as f64) as i32)
+                .max(0)
+                .min(self.extent_cells[0] - 1),
+            ((fraction[1] * self.extent_cells[1] as f64) as i32)
+                .max(0)
+                .min(self.extent_cells[1] - 1),
+            ((fraction[2] * self.extent_cells[2] as f64) as i32)
+                .max(0)
+                .min(self.extent_cells[2] - 1),
         );
-        return self.extent_cells[1] as i32
-            * (self.extent_cells[0] as i32 * (cell_id[0] as i32) + (cell_id[1] as i32))
+        return self.extent_cells[2] as i32
+            * (self.extent_cells[1] as i32 * (cell_id[0] as i32) + (cell_id[1] as i32))
             + (cell_id[2] as i32);
     }
 
