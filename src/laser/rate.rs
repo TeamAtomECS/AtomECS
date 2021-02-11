0c1b63114b29a20230e7a3d8ab654dc3d8d0b46a
Elliot Bentine <7041164+ElliotB256@users.noreply.github.com>
2021-02-11 14:55:35 +0000

    Try to improve vectorization.

--- src/laser/rate.rs
+++ src/laser/rate.rs
@@ -101,22 +101,28 @@ impl<'a> System<'a> for CalculateRateCoefficientsSystem {
         )
             .par_join()
             .for_each(|(detunings, intensities, atominfo, bfield, masks, rates)| {
-                // LLVM should auto vectorize this but does not!
-                for (rate, (detuning, (intensity, mask))) in rates.contents.iter_mut().zip(
-                    detunings
-                        .contents
-                        .iter()
-                        .zip(intensities.contents.iter().zip(masks.contents.iter())),
-                ) {
-                    if !mask.filled {
-                        continue;
-                    }
-
-                    let costheta = if &bfield.field.norm_squared() < &(10.0 * f64::EPSILON) {
+                let costhetas = intensities.contents.iter().map(|intensity| {
+                    if &bfield.field.norm_squared() < &(10.0 * f64::EPSILON) {
                         0.0
                     } else {
                         intensity.direction.dot(&bfield.field.normalize())
-                    };
+                    }
+                });
+
+                // LLVM should auto vectorize this but does not!
+                for (rate, (detuning, (intensity, (costheta, mask)))) in
+                    rates.contents.iter_mut().zip(
+                        detunings.contents.iter().zip(
+                            intensities
+                                .contents
+                                .iter()
+                                .zip(costhetas.zip(masks.contents.iter())),
+                        ),
+                    )
+                {
+                    if !mask.filled {
+                        continue;
+                    }
 
                     let prefactor = atominfo.rate_prefactor * intensity.intensity;
 
