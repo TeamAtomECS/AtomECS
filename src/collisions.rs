65c251575f5f81b3c9575bc1ceea2eba78c40bc9
David Garrick <garrick@physics.ox.ac.uk>
2021-02-11 16:07:15 +0000

    A roughly functioning version of collisions. Includes spatial partitioning but is otherwise unoptimised and untested.

--- src/collisions.rs
+++ src/collisions.rs
@@ -9,74 +9,75 @@ use specs::{Join, Read, ReadStorage, System, WriteStorage};
 use rand::Rng;
 
 
-
-/// A resource that indicates that the simulation should apply scattering
-pub struct ApplyCollisionsOption;
-
-
 /// This system applies scattering to atoms
 /// Uses spatial partitioning for faster calculation
+/// 
 pub struct ApplyCollisionsSystem;
 impl<'a> System<'a> for ApplyCollisionsSystem {
     type SystemData = (
-        WriteStorage<'a, Velocity>,
-        ReadStorage<'a, Mass>,
         ReadStorage<'a, Position>,
-        Option<Read<'a, ApplySWaveOption>,
+        WriteStorage<'a, Velocity>,
     );
 
     fn run(&mut self, (positions, mut velocities): Self::SystemData ){
         
             //make hash table - dividing space up into grid
             let mut map = MultiMap::new();
-            let N = 10; // number of boxes per side
-            let width = 50e-6; // width of each box
+            let N: i32 = 200; // number of boxes per side
+            let width: f64 = 2e-6; // width of each box
+            let mut bin_list = Vec::new();
 
-        for (position,velocity) in (&positions, &velocities) {
 
-            //convert position to box key
-            let rounded = Vector3::new((position.pos[0]/width).floor(),(position.pos[1]/width).floor(),(position.pos[2]/width).floor());
-            key = rounded[0]+N*rounded[1]+N.powf(2)*rounded[2];
+            for (position,velocity) in (&positions, &mut velocities).join() {
 
-            //insert atom velocity into hash with that key
-            map.insert(key, velocity)
-            
-        }
-
-        for key in &map.keys {
-
-            vels = map.get(key);
-
-            for mut vel1 in &vels{
-                for mut vel2 in &vels{
-                    if vel1 == vel2{
-                        break
-                    } else {
-                        vel1, vel2 = do_collision(vel1,vel2);
-                    }
-                }
+                //convert position to box key
+                let rounded = Vector3::new((position.pos[0]/width).floor() as i32,(position.pos[1]/width).floor() as i32,(position.pos[2]/width).floor() as i32);
+                let key = rounded[0]+N*rounded[1]+N.pow(2)*rounded[2];
+    
+                //insert atom velocity into hash with that key
+                map.insert(key, velocity);
+                bin_list.push(key);  
+                
             }
+
+            let mut bin_ids = bin_list.clone();
+            bin_ids.sort();
+            bin_ids.dedup();
+
+
+            for key in &bin_ids {
+                let result = map.get_vec_mut(key);
+                //let mut vels: Vec<_> = (&mut velocities).join().collect();
+
+                let collision_chance = 0.0;
+                let mut rng = rand::thread_rng();
+                
+                match result {
+                    Some(vels) =>
+
+                    for i in 0..vels.len()-1{
+                        for j in i+1..vels.len()-1{
+
+                            let p = rng.gen::<f64>();
+
+                            if p < collision_chance {
+                                let v1 = vels[i].vel;
+                                let v2 = vels[j].vel;
+
+                                let temp = v1;
+
+                                vels[i].vel = v2;
+                                vels[j].vel = temp;
+                                }
+                        
+
+                            }
+                        }
+                    
+                    None => println!("No velocities found.")
+                    }   
+
+                }
         }
 
     }
-}
-
-
-fn do_collision(vel1: &Velocity,vel2: &Velocity) -> (Velocity, Velocity) {
-    let collision_chance = 0.1;
-
-    let mut rng = rand::thread_rng();
-
-    let p = rng.gen::<f64>();
-
-    if p < collision_chance{
-        let vel1_new = vel2;
-        let vel2_new = vel1;
-    } else {
-        let vel1_new = vel1;
-        let vel2_new = vel2;
-    }
-
-    vel1_new, vel2_new
-
-}
