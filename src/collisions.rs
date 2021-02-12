dee5e6a62b876b50b24f36a3fa83bee308bc4ab3
David Garrick <garrick@physics.ox.ac.uk>
2021-02-12 18:12:56 +0000

    Added collisions option in. Also added boundary checking when adding atoms to boxes and centered grid on the origin, so the box ids should be unique per position range.

--- src/collisions.rs
+++ src/collisions.rs
@@ -9,6 +9,9 @@ use specs::{Join, Read, ReadStorage, System, WriteStorage};
 use rand::Rng;
 
 
+/// A resource that indicates that the simulation should apply scattering
+pub struct ApplyCollisionsOption;
+
 /// This system applies scattering to atoms
 /// Uses spatial partitioning for faster calculation
 /// 
@@ -17,67 +20,82 @@ impl<'a> System<'a> for ApplyCollisionsSystem {
     type SystemData = (
         ReadStorage<'a, Position>,
         WriteStorage<'a, Velocity>,
+        Option<Read<'a,ApplyCollisionsOption>>,
     );
 
-    fn run(&mut self, (positions, mut velocities): Self::SystemData ){
+    fn run(&mut self, (positions, mut velocities, collisions_option): Self::SystemData ){
+        match collisions_option {
+            None => (),//(println!("No collisions option enabled")),
+            Some(_) => {
+                //make hash table - dividing space up into grid
+                let mut map = MultiMap::new();
+                let N: i64 = 100; // number of boxes per side
+                let width: f64 = 5e-6; // width of each box
+                let mut bin_list = Vec::new();
+
+
+                for (position,velocity) in (&positions, &mut velocities).join() {
+                    //Assume that atoms that leave the grid are too sparse to collide, so disregard them
+                    let bound = (N as f64)/2.0 * width;
+                    if position.pos[0].abs() > bound {
+                        continue
+                    } else if position.pos[1].abs() > bound {
+                        continue
+                    } else if position.pos[2].abs() > bound {
+                        continue
+                    }
+
+                    //centre grid on origin
+                    let xp = ((position.pos[0]-((N as f64)/2.0).floor())/width).floor() as i64;
+                    let yp = ((position.pos[1]-((N as f64)/2.0).floor())/width).floor() as i64;
+                    let zp = ((position.pos[2]-((N as f64)/2.0).floor())/width).floor() as i64;
+
+                    //convert position to box key
+                    let rounded = Vector3::new(xp,yp,zp);
+                    let key = rounded[0]+N*rounded[1]+N.pow(2)*rounded[2];
+                    
         
-            //make hash table - dividing space up into grid
-            let mut map = MultiMap::new();
-            let N: i32 = 200; // number of boxes per side
-            let width: f64 = 2e-6; // width of each box
-            let mut bin_list = Vec::new();
+                    //insert atom velocity into hash with that key
+                    map.insert(key, velocity);
+                    bin_list.push(key);  
+                    
+                    }
+
+                let mut bin_ids = bin_list.clone();
+                bin_ids.sort();
+                bin_ids.dedup();
 
 
-            for (position,velocity) in (&positions, &mut velocities).join() {
+                for key in &bin_ids {
+                    let result = map.get_vec_mut(key);
 
-                //convert position to box key
-                let rounded = Vector3::new((position.pos[0]/width).floor() as i32,(position.pos[1]/width).floor() as i32,(position.pos[2]/width).floor() as i32);
-                let key = rounded[0]+N*rounded[1]+N.pow(2)*rounded[2];
-    
-                //insert atom velocity into hash with that key
-                map.insert(key, velocity);
-                bin_list.push(key);  
-                
-            }
+                    let collision_chance = 0.1;
+                    let mut rng = rand::thread_rng();
+                    
+                    match result {
+                        Some(vels) =>
+                            for i in 0..vels.len()-1{
+                                for j in i+1..vels.len()-1{
+                                
 
-            let mut bin_ids = bin_list.clone();
-            bin_ids.sort();
-            bin_ids.dedup();
+                                    let p = rng.gen::<f64>();
 
+                                    if p < collision_chance {
+                                        let v1 = vels[i].vel;
+                                        let v2 = vels[j].vel;
 
-            for key in &bin_ids {
-                let result = map.get_vec_mut(key);
-                //let mut vels: Vec<_> = (&mut velocities).join().collect();
+                                        let temp = v1;
 
-                let collision_chance = 0.0;
-                let mut rng = rand::thread_rng();
-                
-                match result {
-                    Some(vels) =>
-
-                    for i in 0..vels.len()-1{
-                        for j in i+1..vels.len()-1{
-
-                            let p = rng.gen::<f64>();
-
-                            if p < collision_chance {
-                                let v1 = vels[i].vel;
-                                let v2 = vels[j].vel;
-
-                                let temp = v1;
-
-                                vels[i].vel = v2;
-                                vels[j].vel = temp;
+                                        vels[i].vel = v2;
+                                        vels[j].vel = temp;
+                                        }
+                                    }
                                 }
                         
-
-                            }
-                        }
-                    
-                    None => println!("No velocities found.")
-                    }   
-
-                }
+                        None => ()//println!("No velocities found")
+                        }   
+                    }
+                }   
+            }
         }
-
     }
