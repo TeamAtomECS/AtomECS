2d16408d0784ffb70ab66ee387f5d4052c8982e7
Elliot Bentine <7041164+ElliotB256@users.noreply.github.com>
2021-04-22 16:22:25 +0100

    Added tests for ramp

--- src/ramp.rs
+++ src/ramp.rs
@@ -32,13 +32,14 @@ where
 {
     pub fn get_value(&mut self, current_time: f64) -> T {
         // check if we need to advance cursor
-        let (t0, _) = &self.keyframes[self.prev];
-        if current_time > *t0 {
-            self.prev = (self.prev + 1).min(self.keyframes.len() - 1);
+        if !self.at_end() {
+            let (t0, _) = &self.keyframes[self.prev + 1];
+            if current_time > *t0 {
+                self.prev = (self.prev + 1).min(self.keyframes.len() - 1);
+            }
         }
-
-        // if at end, just return last frame value.
-        if self.prev == self.keyframes.len() - 1 {
+        // if at end, return last frame value.
+        if self.at_end() {
             let (_, last) = &self.keyframes[self.prev];
             return last.clone();
         }
@@ -48,6 +49,10 @@ where
         let (t2, val_b) = &self.keyframes[self.prev + 1];
         let amount = (current_time - t1) / (t2 - t1);
         return val_a.lerp(&val_b, amount);
+    }
+
+    fn at_end(&self) -> bool {
+        return self.prev == self.keyframes.len() - 1;
     }
 }
 
@@ -83,5 +88,71 @@ where
         for (ramp, comp) in (&mut ramps, &mut comps).join() {
             comp.clone_from(&ramp.get_value(current_time));
         }
+    }
+}
+
+pub mod tests {
+    use super::*;
+    extern crate specs;
+    use specs::{Component, HashMapStorage};
+
+    #[derive(Clone)]
+    struct ALerpComp {
+        value: f64,
+    }
+
+    impl Component for ALerpComp {
+        type Storage = HashMapStorage<Self>;
+    }
+
+    impl Lerp<ALerpComp> for ALerpComp {
+        fn lerp(&self, other: &Self, amount: f64) -> Self {
+            ALerpComp {
+                value: self.value * (1.0 - amount) + other.value * amount,
+            }
+        }
+    }
+
+    #[test]
+    fn test_ramp() {
+        let comp_a = ALerpComp { value: 0.0 };
+        let comp_b = ALerpComp { value: 1.0 };
+
+        let mut frames = Vec::new();
+        frames.push((0.0, comp_a.clone()));
+        frames.push((1.0, comp_b.clone()));
+        let mut ramp = Ramp {
+            prev: 0,
+            keyframes: frames,
+        };
+
+        let a = ramp.get_value(0.0);
+        assert!(
+            a.value < std::f64::EPSILON,
+            "incorrect: a.value={}, target={}",
+            a.value,
+            0.0
+        );
+        let b = ramp.get_value(0.5);
+        assert!(
+            (0.5 - b.value).abs() < std::f64::EPSILON,
+            "incorrect: value={}, target={}",
+            b.value,
+            0.5
+        );
+        let c = ramp.get_value(1.0);
+        assert!(
+            (1.0 - c.value).abs() < std::f64::EPSILON,
+            "incorrect: value={}, target={}",
+            c.value,
+            1.0
+        );
+        let d = ramp.get_value(2.0);
+        assert!(
+            (1.0 - d.value).abs() < std::f64::EPSILON,
+            "incorrect: value={}, target={}",
+            d.value,
+            1.0
+        );
     }
 }
