528c3f6bd6c99a4f265df905f2f2bad04dd596bf
Elliot Bentine <7041164+ElliotB256@users.noreply.github.com>
2021-05-08 19:03:10 +0100

    Added functionality for keyframe ramping of components.
    The ramps can be performed for any component that implements the `Lerp` trait. To define a ramp for a component `C`, add a `Ramp<C>` to the same entity, and add a `RampUpdateSystem<C>` to the dispatcher.
    
    Also added unit tests, which include a minimal example.
    
    Signed-off-by: Elliot Bentine <7041164+ElliotB256@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 //! Main program does nothing
 
-extern crate atomecs as lib;
-
 fn main() {
     println!("Welcome to AtomECS! Check out the repository readme for more details, or see the examples.");
 }
