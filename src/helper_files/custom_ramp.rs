022786d428369a76fd6a95dc15b8f4e9db868935
MauriceZeuner <physics@mzeu.eu>
2021-07-15 10:28:50 +0100

    added starting time to eth ramp

--- src/helper_files/custom_ramp.rs
+++ src/helper_files/custom_ramp.rs
@@ -3,13 +3,14 @@ use crate::ramp::Ramp;
 
 pub fn get_gaussian_eth_ramp(
     duration: f64,
+    starting_time: f64,
     steps: i64,
     distance: f64,
     gaussian: GaussianBeam,
 ) -> Ramp<GaussianBeam> {
     let mut frames = Vec::new();
     for i in 0..steps {
-        let t = i as f64 * duration / steps as f64;
+        let t = i as f64 * duration / steps as f64 + starting_time;
         frames.push((
             t,
             GaussianBeam {
@@ -29,6 +30,5 @@ pub fn get_gaussian_eth_ramp(
 pub fn focus_eth_ramp(t: f64, duration: &f64, distance: &f64) -> f64 {
     let a0 = 6.0 * distance / (duration.powf(2.0));
     let focus = 1. / 2. * a0 * t.powf(2.0) - 1. / (3. * duration) * a0 * t.powf(3.0);
-    println!("focus is: {}", focus);
     return focus;
 }
