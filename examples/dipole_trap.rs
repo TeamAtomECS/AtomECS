bb47a962f3638f2a45be5d4b5e5d6c2650ee96fd
Elliot Bentine <7041164+ElliotB256@users.noreply.github.com>
2021-07-21 23:16:24 +0100

    rename minimum example of ODT, delete extra example

--- examples/dipole_trap.rs
+++ examples/dipole_trap.rs
@@ -1,22 +1,18 @@
-//! Loading a Sr cross beam dipole trap from center.
-use specs::prelude::*;
+//! Single particle in a cross beam optical dipole trap
 extern crate atomecs as lib;
 extern crate nalgebra;
+use lib::atom;
 use lib::atom::Atom;
-use lib::atom::{AtomicTransition, Position, Velocity};
-use lib::atom_sources::central_creator::CentralCreator;
-use lib::atom_sources::emit::AtomNumberToEmit;
-use lib::atom_sources::mass::{MassDistribution, MassRatio};
-use lib::destructor::ToBeDestroyed;
+use lib::dipole;
 use lib::ecs;
 use lib::integrator::Timestep;
 use lib::laser;
 use lib::laser::gaussian::GaussianBeam;
 use lib::output::file;
 use lib::output::file::{Text, XYZ};
-use lib::shapes::Cuboid;
-use lib::sim_region::{SimulationVolume, VolumeType};
 use nalgebra::Vector3;
+use specs::prelude::*;
+use specs::{Builder, World};
 use std::time::Instant;
 
 fn main() {
@@ -30,41 +26,42 @@ fn main() {
 
     // Configure simulation output.
     builder = builder.with(
-        file::new::<Position, Text>("pos_dipole.txt".to_string(), 100),
+        file::new::<atom::Position, Text>("pos_dipole.txt".to_string(), 100),
         "",
         &[],
     );
     builder = builder.with(
-        file::new::<Velocity, Text>("vel_dipole.txt".to_string(), 100),
+        file::new::<atom::Velocity, Text>("vel_dipole.txt".to_string(), 100),
         "",
         &[],
     );
     builder = builder.with(
-        file::new_with_filter::<Position, XYZ, Atom>("position.xyz".to_string(), 100),
+        file::new_with_filter::<atom::Position, XYZ, Atom>("position.xyz".to_string(), 100),
         "",
         &[],
     );
 
     let mut dispatcher = builder.build();
     dispatcher.setup(&mut world);
-
     // Create dipole laser.
+
     let power = 10.0;
     let e_radius = 60.0e-6 / (2.0_f64.sqrt());
+    let wavelength = 1064.0e-9;
 
     let gaussian_beam = GaussianBeam {
         intersection: Vector3::new(0.0, 0.0, 0.0),
         e_radius: e_radius,
         power: power,
         direction: Vector3::x(),
-        rayleigh_range: lib::laser::gaussian::calculate_rayleigh_range(&1064.0e-9, &e_radius),
+        rayleigh_range: crate::laser::gaussian::calculate_rayleigh_range(&wavelength, &e_radius),
         ellipticity: 0.0,
     };
     world
         .create_entity()
         .with(gaussian_beam)
         .with(laser::dipole_beam::DipoleLight {
-            wavelength: 1064.0e-9,
+            wavelength: wavelength,
         })
         .with(laser::frame::Frame {
             x_vector: Vector3::y(),
@@ -77,66 +74,39 @@ fn main() {
         e_radius: e_radius,
         power: power,
         direction: Vector3::y(),
-        rayleigh_range: lib::laser::gaussian::calculate_rayleigh_range(&1064.0e-9, &e_radius),
+        rayleigh_range: crate::laser::gaussian::calculate_rayleigh_range(&wavelength, &e_radius),
         ellipticity: 0.0,
     };
     world
         .create_entity()
         .with(gaussian_beam)
         .with(laser::dipole_beam::DipoleLight {
-            wavelength: 1064.0e-9,
+            wavelength: wavelength,
         })
         .with(laser::frame::Frame {
             x_vector: Vector3::x(),
             y_vector: Vector3::z(),
         })
         .build();
-    // creating the entity that represents the source
-    //
-    // contains a central creator
-    let number_to_emit = 1_000;
-    let size_of_cube = 1.0e-5;
-    let speed = 0.1; // m/s
-
-    world
-        .create_entity()
-        .with(CentralCreator::new_uniform_cubic(size_of_cube, speed))
-        .with(Position {
-            pos: Vector3::new(0.0, 0.0, 0.0),
-        })
-        .with(MassDistribution::new(vec![MassRatio {
-            mass: 87.0,
-            ratio: 1.0,
-        }]))
-        .with(AtomicTransition::strontium_red())
-        .with(AtomNumberToEmit {
-            number: number_to_emit,
-        })
-        .with(ToBeDestroyed)
-        .build();
 
     // Define timestep
-    world.insert(Timestep { delta: 1.0e-6 });
-    // Use a simulation bound so that atoms that escape the capture region are deleted from the simulation
+    world.insert(Timestep { delta: 1.0e-5 });
+
+    // Create a single test atom
     world
         .create_entity()
-        .with(Position {
-            pos: Vector3::new(0.0, 0.0, 0.0),
+        .with(atom::Mass { value: 87.0 })
+        .with(atom::Force::new())
+        .with(atom::Position {
+            pos: Vector3::new(-5.0e-6, 5.0e-6, 5.0e-6),
         })
-        .with(Cuboid {
-            half_width: Vector3::new(0.0005, 0.0005, 0.0005), //(0.1, 0.01, 0.01)
+        .with(atom::Velocity {
+            vel: Vector3::new(0.0, 0.0, 0.0),
         })
-        .with(SimulationVolume {
-            volume_type: VolumeType::Inclusive,
-        })
+        .with(dipole::atom::AtomicDipoleTransition::strontium())
+        .with(atom::Atom)
+        .with(lib::initiate::NewlyCreated)
         .build();
-
-    let mut switcher_system =
-        atomecs::dipole::transition_switcher::AttachAtomicDipoleTransitionToAtomsSystem;
-
-    dispatcher.dispatch(&mut world);
-    world.maintain();
-    switcher_system.run_now(&world);
 
     // Run the simulation for a number of steps.
     for _i in 0..100_000 {
