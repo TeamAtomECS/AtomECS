e4e703f86b92c967832d38f453e2969012b45013
minghuaw <michael.wu1107@gmail.com>
2021-12-06 18:18:28 -0800

    changed assert! to comply to rust edition 2021 panic macro consistency

--- src/integrator.rs
+++ src/integrator.rs
@@ -12,7 +12,7 @@ use specs::prelude::*;
 
 /// Tracks the number of the current integration step.
 pub struct Step {
-	pub n: u64,
+    pub n: u64,
 }
 
 /// The timestep used for the integration.
@@ -23,8 +23,8 @@ pub struct Step {
 /// Decreasing the timestep further will not improve the accuracy, and will require more integration steps
 /// to simulate the same total simulation time.
 pub struct Timestep {
-	/// Duration of the simulation timestep, in SI units of seconds.
-	pub delta: f64,
+    /// Duration of the simulation timestep, in SI units of seconds.
+    pub delta: f64,
 }
 
 /// # Euler Integration
@@ -37,25 +37,25 @@ pub struct Timestep {
 pub struct EulerIntegrationSystem;
 
 impl<'a> System<'a> for EulerIntegrationSystem {
-	type SystemData = (
-		WriteStorage<'a, Position>,
-		WriteStorage<'a, Velocity>,
-		ReadExpect<'a, Timestep>,
-		WriteExpect<'a, Step>,
-		ReadStorage<'a, Force>,
-		ReadStorage<'a, Mass>,
-	);
+    type SystemData = (
+        WriteStorage<'a, Position>,
+        WriteStorage<'a, Velocity>,
+        ReadExpect<'a, Timestep>,
+        WriteExpect<'a, Step>,
+        ReadStorage<'a, Force>,
+        ReadStorage<'a, Mass>,
+    );
 
-	fn run(&mut self, (mut pos, mut vel, t, mut step, force, mass): Self::SystemData) {
-		use rayon::prelude::*;
+    fn run(&mut self, (mut pos, mut vel, t, mut step, force, mass): Self::SystemData) {
+        use rayon::prelude::*;
 
-		step.n = step.n + 1;
-		(&mut vel, &mut pos, &force, &mass).par_join().for_each(
-			|(mut vel, mut pos, force, mass)| {
-				euler_update(&mut vel, &mut pos, &force, &mass, t.delta);
-			},
-		);
-	}
+        step.n = step.n + 1;
+        (&mut vel, &mut pos, &force, &mass).par_join().for_each(
+            |(mut vel, mut pos, force, mass)| {
+                euler_update(&mut vel, &mut pos, &force, &mass, t.delta);
+            },
+        );
+    }
 }
 
 pub const INTEGRATE_POSITION_SYSTEM_NAME: &str = "integrate_position";
@@ -68,31 +68,31 @@ pub const INTEGRATE_POSITION_SYSTEM_NAME: &str = "integrate_position";
 /// The timestep duration is specified by the [Timestep](struct.Timestep.html) system resource.
 pub struct VelocityVerletIntegratePositionSystem;
 impl<'a> System<'a> for VelocityVerletIntegratePositionSystem {
-	type SystemData = (
-		WriteStorage<'a, Position>,
-		ReadStorage<'a, Velocity>,
-		ReadExpect<'a, Timestep>,
-		WriteExpect<'a, Step>,
-		ReadStorage<'a, Force>,
-		WriteStorage<'a, OldForce>,
-		ReadStorage<'a, Mass>,
-	);
+    type SystemData = (
+        WriteStorage<'a, Position>,
+        ReadStorage<'a, Velocity>,
+        ReadExpect<'a, Timestep>,
+        WriteExpect<'a, Step>,
+        ReadStorage<'a, Force>,
+        WriteStorage<'a, OldForce>,
+        ReadStorage<'a, Mass>,
+    );
 
-	fn run(&mut self, (mut pos, vel, t, mut step, force, mut oldforce, mass): Self::SystemData) {
-		use rayon::prelude::*;
+    fn run(&mut self, (mut pos, vel, t, mut step, force, mut oldforce, mass): Self::SystemData) {
+        use rayon::prelude::*;
 
-		step.n = step.n + 1;
-		let dt = t.delta;
+        step.n = step.n + 1;
+        let dt = t.delta;
 
-		(&mut pos, &vel, &mut oldforce, &force, &mass)
-			.par_join()
-			.for_each(|(mut pos, vel, mut oldforce, force, mass)| {
-				pos.pos = pos.pos
-					+ vel.vel * dt + force.force / (constant::AMU * mass.value) / 2.0
-					* dt * dt;
-				oldforce.0 = *force;
-			});
-	}
+        (&mut pos, &vel, &mut oldforce, &force, &mass)
+            .par_join()
+            .for_each(|(mut pos, vel, mut oldforce, force, mass)| {
+                pos.pos = pos.pos
+                    + vel.vel * dt
+                    + force.force / (constant::AMU * mass.value) / 2.0 * dt * dt;
+                oldforce.0 = *force;
+            });
+    }
 }
 
 pub const INTEGRATE_VELOCITY_SYSTEM_NAME: &str = "integrate_velocity";
@@ -104,253 +104,253 @@ pub const INTEGRATE_VELOCITY_SYSTEM_NAME: &str = "integrate_velocity";
 /// The timestep duration is specified by the [Timestep](struct.Timestep.html) system resource
 pub struct VelocityVerletIntegrateVelocitySystem;
 impl<'a> System<'a> for VelocityVerletIntegrateVelocitySystem {
-	type SystemData = (
-		WriteStorage<'a, Velocity>,
-		ReadExpect<'a, Timestep>,
-		ReadStorage<'a, Force>,
-		ReadStorage<'a, OldForce>,
-		ReadStorage<'a, Mass>,
-	);
+    type SystemData = (
+        WriteStorage<'a, Velocity>,
+        ReadExpect<'a, Timestep>,
+        ReadStorage<'a, Force>,
+        ReadStorage<'a, OldForce>,
+        ReadStorage<'a, Mass>,
+    );
 
-	fn run(&mut self, (mut vel, t, force, oldforce, mass): Self::SystemData) {
-		use rayon::prelude::*;
+    fn run(&mut self, (mut vel, t, force, oldforce, mass): Self::SystemData) {
+        use rayon::prelude::*;
 
-		let dt = t.delta;
+        let dt = t.delta;
 
-		(&mut vel, &force, &oldforce, &mass).par_join().for_each(
-			|(mut vel, force, oldforce, mass)| {
-				vel.vel = vel.vel
-					+ (force.force + oldforce.0.force) / (constant::AMU * mass.value) / 2.0 * dt;
-			},
-		);
-	}
+        (&mut vel, &force, &oldforce, &mass).par_join().for_each(
+            |(mut vel, force, oldforce, mass)| {
+                vel.vel = vel.vel
+                    + (force.force + oldforce.0.force) / (constant::AMU * mass.value) / 2.0 * dt;
+            },
+        );
+    }
 }
 
 /// Adds [OldForce](OldForce.struct.html) components to newly created atoms.
 pub struct AddOldForceToNewAtomsSystem;
 impl<'a> System<'a> for AddOldForceToNewAtomsSystem {
-	type SystemData = (
-		Entities<'a>,
-		ReadStorage<'a, NewlyCreated>,
-		ReadStorage<'a, OldForce>,
-		Read<'a, LazyUpdate>,
-	);
-	fn run(&mut self, (ent, newly_created, oldforce, updater): Self::SystemData) {
-		for (ent, _, _) in (&ent, &newly_created, !&oldforce).join() {
-			updater.insert(ent, OldForce::default());
-		}
-	}
+    type SystemData = (
+        Entities<'a>,
+        ReadStorage<'a, NewlyCreated>,
+        ReadStorage<'a, OldForce>,
+        Read<'a, LazyUpdate>,
+    );
+    fn run(&mut self, (ent, newly_created, oldforce, updater): Self::SystemData) {
+        for (ent, _, _) in (&ent, &newly_created, !&oldforce).join() {
+            updater.insert(ent, OldForce::default());
+        }
+    }
 }
 
 /// Stores the value of the force calculation from the previous frame.
 pub struct OldForce(Force);
 impl Component for OldForce {
-	type Storage = VecStorage<OldForce>;
+    type Storage = VecStorage<OldForce>;
 }
 impl Default for OldForce {
-	fn default() -> Self {
-		OldForce { 0: Force::new() }
-	}
+    fn default() -> Self {
+        OldForce { 0: Force::new() }
+    }
 }
 
 /// Performs the euler method to update [Velocity](struct.Velocity.html) and [Position](struct.Position.html) given an applied [Force](struct.Force.html).
 fn euler_update(vel: &mut Velocity, pos: &mut Position, force: &Force, mass: &Mass, dt: f64) {
-	pos.pos = pos.pos + vel.vel * dt;
-	vel.vel = vel.vel + force.force * dt / (constant::AMU * mass.value);
+    pos.pos = pos.pos + vel.vel * dt;
+    vel.vel = vel.vel + force.force * dt / (constant::AMU * mass.value);
 }
 
 pub mod tests {
-	#[allow(unused_imports)]
-	use super::*;
-	extern crate specs;
-	#[allow(unused_imports)]
-	use specs::{Builder, DispatcherBuilder, World};
+    #[allow(unused_imports)]
+    use super::*;
+    extern crate specs;
+    #[allow(unused_imports)]
+    use specs::{Builder, DispatcherBuilder, World};
 
-	extern crate nalgebra;
-	#[allow(unused_imports)]
-	use nalgebra::Vector3;
+    extern crate nalgebra;
+    #[allow(unused_imports)]
+    use nalgebra::Vector3;
 
-	#[test]
-	fn test_euler() {
-		let mut pos = Position {
-			pos: Vector3::new(1., 1., 1.),
-		};
-		let mut vel = Velocity {
-			vel: Vector3::new(0., 1., 0.),
-		};
-		let time = 1.;
-		let mass = Mass {
-			value: 1. / constant::AMU,
-		};
-		let force = Force {
-			force: Vector3::new(1., 1., 1.),
-		};
-		euler_update(&mut vel, &mut pos, &force, &mass, time);
-		assert_eq!(vel.vel, Vector3::new(1., 2., 1.));
-		assert_eq!(pos.pos, Vector3::new(1., 2., 1.));
-	}
+    #[test]
+    fn test_euler() {
+        let mut pos = Position {
+            pos: Vector3::new(1., 1., 1.),
+        };
+        let mut vel = Velocity {
+            vel: Vector3::new(0., 1., 0.),
+        };
+        let time = 1.;
+        let mass = Mass {
+            value: 1. / constant::AMU,
+        };
+        let force = Force {
+            force: Vector3::new(1., 1., 1.),
+        };
+        euler_update(&mut vel, &mut pos, &force, &mass, time);
+        assert_eq!(vel.vel, Vector3::new(1., 2., 1.));
+        assert_eq!(pos.pos, Vector3::new(1., 2., 1.));
+    }
 
-	/// Tests the [EulerIntegrationSystem] by creating a mock world and integrating the trajectory of one entity.
-	#[test]
-	fn test_euler_integration() {
-		let mut world = World::new();
+    /// Tests the [EulerIntegrationSystem] by creating a mock world and integrating the trajectory of one entity.
+    #[test]
+    fn test_euler_integration() {
+        let mut world = World::new();
 
-		let mut dispatcher = DispatcherBuilder::new()
-			.with(EulerIntegrationSystem, "integrator", &[])
-			.build();
-		dispatcher.setup(&mut world);
+        let mut dispatcher = DispatcherBuilder::new()
+            .with(EulerIntegrationSystem, "integrator", &[])
+            .build();
+        dispatcher.setup(&mut world);
 
-		// create a particle with known force and mass
-		let force = Vector3::new(1.0, 0.0, 0.0);
-		let mass = 1.0;
-		let atom = world
-			.create_entity()
-			.with(Position {
-				pos: Vector3::new(0.0, 0.0, 0.0),
-			})
-			.with(Velocity {
-				vel: Vector3::new(0.0, 0.0, 0.0),
-			})
-			.with(Force { force: force })
-			.with(Mass {
-				value: mass / constant::AMU,
-			})
-			.build();
+        // create a particle with known force and mass
+        let force = Vector3::new(1.0, 0.0, 0.0);
+        let mass = 1.0;
+        let atom = world
+            .create_entity()
+            .with(Position {
+                pos: Vector3::new(0.0, 0.0, 0.0),
+            })
+            .with(Velocity {
+                vel: Vector3::new(0.0, 0.0, 0.0),
+            })
+            .with(Force { force: force })
+            .with(Mass {
+                value: mass / constant::AMU,
+            })
+            .build();
 
-		let dt = 1.0e-3;
-		world.insert(Timestep { delta: dt });
-		world.insert(Step { n: 0 });
+        let dt = 1.0e-3;
+        world.insert(Timestep { delta: dt });
+        world.insert(Step { n: 0 });
 
-		// run simulation loop 1_000 times.
-		let n_steps = 1_000;
-		for _i in 0..n_steps {
-			dispatcher.dispatch(&mut world);
-			world.maintain();
-		}
+        // run simulation loop 1_000 times.
+        let n_steps = 1_000;
+        for _i in 0..n_steps {
+            dispatcher.dispatch(&mut world);
+            world.maintain();
+        }
 
-		let a = force / mass;
-		let expected_v = a * (n_steps as f64 * dt);
+        let a = force / mass;
+        let expected_v = a * (n_steps as f64 * dt);
 
-		assert_approx_eq::assert_approx_eq!(
-			expected_v.norm(),
-			world
-				.read_storage::<Velocity>()
-				.get(atom)
-				.expect("atom not found.")
-				.vel
-				.norm(),
-			expected_v.norm() * 0.01
-		);
+        assert_approx_eq::assert_approx_eq!(
+            expected_v.norm(),
+            world
+                .read_storage::<Velocity>()
+                .get(atom)
+                .expect("atom not found.")
+                .vel
+                .norm(),
+            expected_v.norm() * 0.01
+        );
 
-		let expected_x = a * (n_steps as f64 * dt).powi(2) / 2.0;
-		assert_approx_eq::assert_approx_eq!(
-			expected_x.norm(),
-			world
-				.read_storage::<Position>()
-				.get(atom)
-				.expect("atom not found.")
-				.pos
-				.norm(),
-			expected_x.norm() * 0.01
-		);
-	}
+        let expected_x = a * (n_steps as f64 * dt).powi(2) / 2.0;
+        assert_approx_eq::assert_approx_eq!(
+            expected_x.norm(),
+            world
+                .read_storage::<Position>()
+                .get(atom)
+                .expect("atom not found.")
+                .pos
+                .norm(),
+            expected_x.norm() * 0.01
+        );
+    }
 
-	#[test]
-	fn test_add_old_force_system() {
-		let mut test_world = World::new();
+    #[test]
+    fn test_add_old_force_system() {
+        let mut test_world = World::new();
 
-		let mut dispatcher = DispatcherBuilder::new()
-			.with(AddOldForceToNewAtomsSystem, "", &[])
-			.build();
-		dispatcher.setup(&mut test_world);
-		test_world.register::<OldForce>();
+        let mut dispatcher = DispatcherBuilder::new()
+            .with(AddOldForceToNewAtomsSystem, "", &[])
+            .build();
+        dispatcher.setup(&mut test_world);
+        test_world.register::<OldForce>();
 
-		let test_entity = test_world.create_entity().with(NewlyCreated {}).build();
+        let test_entity = test_world.create_entity().with(NewlyCreated {}).build();
 
-		dispatcher.dispatch(&mut test_world);
-		test_world.maintain();
+        dispatcher.dispatch(&mut test_world);
+        test_world.maintain();
 
-		let old_forces = test_world.read_storage::<OldForce>();
-		assert_eq!(
-			old_forces.contains(test_entity),
-			true,
-			"OldForce component not added to test entity."
-		);
-	}
+        let old_forces = test_world.read_storage::<OldForce>();
+        assert_eq!(
+            old_forces.contains(test_entity),
+            true,
+            "OldForce component not added to test entity."
+        );
+    }
 
-	#[test]
-	fn test_velocity_verlet_integration() {
-		let mut world = World::new();
+    #[test]
+    fn test_velocity_verlet_integration() {
+        let mut world = World::new();
 
-		let mut dispatcher = DispatcherBuilder::new()
-			.with(
-				VelocityVerletIntegratePositionSystem,
-				"integrate_position",
-				&[],
-			)
-			.with(
-				VelocityVerletIntegrateVelocitySystem,
-				"integrate_velocity",
-				&["integrate_position"],
-			)
-			.build();
-		dispatcher.setup(&mut world);
+        let mut dispatcher = DispatcherBuilder::new()
+            .with(
+                VelocityVerletIntegratePositionSystem,
+                "integrate_position",
+                &[],
+            )
+            .with(
+                VelocityVerletIntegrateVelocitySystem,
+                "integrate_velocity",
+                &["integrate_position"],
+            )
+            .build();
+        dispatcher.setup(&mut world);
 
-		// create a particle with known force and mass
-		let force = Vector3::new(1.0, 0.0, 0.0);
-		let mass = 1.0;
-		let atom = world
-			.create_entity()
-			.with(Position {
-				pos: Vector3::new(0.0, 0.0, 0.0),
-			})
-			.with(Velocity {
-				vel: Vector3::new(0.0, 0.0, 0.0),
-			})
-			.with(Force { force: force })
-			.with(OldForce {
-				0: Force { force: force },
-			})
-			.with(Mass {
-				value: mass / constant::AMU,
-			})
-			.build();
+        // create a particle with known force and mass
+        let force = Vector3::new(1.0, 0.0, 0.0);
+        let mass = 1.0;
+        let atom = world
+            .create_entity()
+            .with(Position {
+                pos: Vector3::new(0.0, 0.0, 0.0),
+            })
+            .with(Velocity {
+                vel: Vector3::new(0.0, 0.0, 0.0),
+            })
+            .with(Force { force: force })
+            .with(OldForce {
+                0: Force { force: force },
+            })
+            .with(Mass {
+                value: mass / constant::AMU,
+            })
+            .build();
 
-		let dt = 1.0e-3;
-		world.insert(Timestep { delta: dt });
-		world.insert(Step { n: 0 });
+        let dt = 1.0e-3;
+        world.insert(Timestep { delta: dt });
+        world.insert(Step { n: 0 });
 
-		// run simulation loop 1_000 times.
-		let n_steps = 1_000;
-		for _i in 0..n_steps {
-			dispatcher.dispatch(&mut world);
-			world.maintain();
-		}
+        // run simulation loop 1_000 times.
+        let n_steps = 1_000;
+        for _i in 0..n_steps {
+            dispatcher.dispatch(&mut world);
+            world.maintain();
+        }
 
-		let a = force / mass;
-		let expected_v = a * (n_steps as f64 * dt);
+        let a = force / mass;
+        let expected_v = a * (n_steps as f64 * dt);
 
-		assert_approx_eq::assert_approx_eq!(
-			expected_v.norm(),
-			world
-				.read_storage::<Velocity>()
-				.get(atom)
-				.expect("atom not found.")
-				.vel
-				.norm(),
-			expected_v.norm() * 0.01
-		);
+        assert_approx_eq::assert_approx_eq!(
+            expected_v.norm(),
+            world
+                .read_storage::<Velocity>()
+                .get(atom)
+                .expect("atom not found.")
+                .vel
+                .norm(),
+            expected_v.norm() * 0.01
+        );
 
-		let expected_x = a * (n_steps as f64 * dt).powi(2) / 2.0;
-		assert_approx_eq::assert_approx_eq!(
-			expected_x.norm(),
-			world
-				.read_storage::<Position>()
-				.get(atom)
-				.expect("atom not found.")
-				.pos
-				.norm(),
-			expected_x.norm() * 0.01
-		);
-	}
+        let expected_x = a * (n_steps as f64 * dt).powi(2) / 2.0;
+        assert_approx_eq::assert_approx_eq!(
+            expected_x.norm(),
+            world
+                .read_storage::<Position>()
+                .get(atom)
+                .expect("atom not found.")
+                .pos
+                .norm(),
+            expected_x.norm() * 0.01
+        );
+    }
 }
--- src/maths.rs
+++ src/maths.rs
@@ -15,17 +15,17 @@ use nalgebra::Vector3;
 ///
 /// `dir`: vector pointing along the line.
 pub fn get_relative_coordinates_line_point(
-	pos: &Vector3<f64>,
-	line_point: &Vector3<f64>,
-	dir: &Vector3<f64>,
-	frame: &crate::laser::frame::Frame,
+    pos: &Vector3<f64>,
+    line_point: &Vector3<f64>,
+    dir: &Vector3<f64>,
+    frame: &crate::laser::frame::Frame,
 ) -> (f64, f64, f64) {
-	let rela_cood = pos - line_point;
-	let z = rela_cood.dot(&dir) / dir.norm();
-	let r_vec = rela_cood - z * dir;
-	let x = r_vec.dot(&frame.x_vector);
-	let y = r_vec.dot(&frame.y_vector);
-	(x, y, z)
+    let rela_cood = pos - line_point;
+    let z = rela_cood.dot(&dir) / dir.norm();
+    let r_vec = rela_cood - z * dir;
+    let x = r_vec.dot(&frame.x_vector);
+    let y = r_vec.dot(&frame.y_vector);
+    (x, y, z)
 }
 
 /// Get miniminum distance between a point and a line.
@@ -38,33 +38,33 @@ pub fn get_relative_coordinates_line_point(
 ///
 /// `dir`: vector pointing along the line.
 pub fn get_minimum_distance_line_point(
-	pos: &Vector3<f64>,
-	line_point: &Vector3<f64>,
-	dir: &Vector3<f64>,
+    pos: &Vector3<f64>,
+    line_point: &Vector3<f64>,
+    dir: &Vector3<f64>,
 ) -> (f64, f64) {
-	let rela_cood = pos - line_point;
-	let distance = (dir.cross(&rela_cood) / dir.norm()).norm();
-	let z = rela_cood.dot(&dir) / dir.norm();
-	(distance, z)
+    let rela_cood = pos - line_point;
+    let distance = (dir.cross(&rela_cood) / dir.norm()).norm();
+    let z = rela_cood.dot(&dir) / dir.norm();
+    (distance, z)
 }
 
 /// A normalised gaussian distribution.
 ///
 /// The distribution is normalised such that the 2D area underneath a gaussian dist with sigma_x=sigma_y=std is equal to 1.
 pub fn gaussian_dis(std: f64, distance_squared: f64) -> f64 {
-	1.0 / (2.0 * PI * std * std) * EXP.powf(-distance_squared / 2.0 / (std * std))
+    1.0 / (2.0 * PI * std * std) * EXP.powf(-distance_squared / 2.0 / (std * std))
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn test_minimum_distance_line_point() {
-		let pos = Vector3::new(1., 1., 1.);
-		let centre = Vector3::new(0., 1., 1.);
-		let dir = Vector3::new(1., 2., 2.);
-		let (distance, _) = get_minimum_distance_line_point(&pos, &centre, &dir);
-		assert!(distance > 0.942, distance < 0.943);
-	}
+    #[test]
+    fn test_minimum_distance_line_point() {
+        let pos = Vector3::new(1., 1., 1.);
+        let centre = Vector3::new(0., 1., 1.);
+        let dir = Vector3::new(1., 2., 2.);
+        let (distance, _) = get_minimum_distance_line_point(&pos, &centre, &dir);
+        assert!(distance > 0.942, "{}", distance < 0.943);
+    }
 }
