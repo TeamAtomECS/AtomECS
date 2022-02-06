5a37367b5dacc7a6edb5b6317a8e297a67923040
MauriceZeuner <physics@mzeu.eu>
2022-02-06 19:32:50 +0100

    set dipole test file to comparable values

--- examples/dipole_trap.rs
+++ examples/dipole_trap.rs
@@ -1,36 +1,45 @@
 //! Single particle in a cross beam optical dipole trap
 extern crate atomecs as lib;
 extern crate nalgebra;
-use lib::atom::{self, Position, Velocity};
 use lib::atom::Atom;
+use lib::atom::{self, Position, Velocity};
 use lib::dipole::{self, DipolePlugin};
 use lib::integrator::Timestep;
-use lib::laser::{self, LaserPlugin};
 use lib::laser::gaussian::GaussianBeam;
-use lib::output::file::{FileOutputPlugin};
+use lib::laser::{self, LaserPlugin};
+use lib::output::file::FileOutputPlugin;
 use lib::output::file::{Text, XYZ};
 use lib::simulation::SimulationBuilder;
 use nalgebra::Vector3;
 use specs::prelude::*;
 use std::time::Instant;
 
-const BEAM_NUMBER: usize = 2;
+const BEAM_NUMBER: usize = 1;
 
 fn main() {
     let now = Instant::now();
 
     // Configure simulation output.
     let mut sim_builder = SimulationBuilder::default();
-    sim_builder.add_plugin(LaserPlugin::<{BEAM_NUMBER}>);
-    sim_builder.add_plugin(DipolePlugin::<{BEAM_NUMBER}>);
-    sim_builder.add_plugin(FileOutputPlugin::<Position, Text, Atom>::new("pos.txt".to_string(), 100));
-    sim_builder.add_plugin(FileOutputPlugin::<Velocity, Text, Atom>::new("vel.txt".to_string(), 100));
-    sim_builder.add_plugin(FileOutputPlugin::<Position, XYZ, Atom>::new("position.xyz".to_string(), 100));
+    sim_builder.add_plugin(LaserPlugin::<{ BEAM_NUMBER }>);
+    sim_builder.add_plugin(DipolePlugin::<{ BEAM_NUMBER }>);
+    sim_builder.add_plugin(FileOutputPlugin::<Position, Text, Atom>::new(
+        "pos.txt".to_string(),
+        100,
+    ));
+    sim_builder.add_plugin(FileOutputPlugin::<Velocity, Text, Atom>::new(
+        "vel.txt".to_string(),
+        100,
+    ));
+    sim_builder.add_plugin(FileOutputPlugin::<Position, XYZ, Atom>::new(
+        "position.xyz".to_string(),
+        100,
+    ));
     let mut sim = sim_builder.build();
 
     // Create dipole laser.
-    let power = 10.0;
-    let e_radius = 60.0e-6 / (2.0_f64.sqrt());
+    let power = 7.0;
+    let e_radius = 50.0e-6 / (2.0_f64.sqrt());
     let wavelength = 1064.0e-9;
 
     let gaussian_beam = GaussianBeam {
@@ -47,24 +56,6 @@ fn main() {
         .with(dipole::DipoleLight { wavelength })
         .with(laser::frame::Frame {
             x_vector: Vector3::y(),
-            y_vector: Vector3::z(),
-        })
-        .build();
-
-    let gaussian_beam = GaussianBeam {
-        intersection: Vector3::new(0.0, 0.0, 0.0),
-        e_radius,
-        power,
-        direction: Vector3::y(),
-        rayleigh_range: crate::laser::gaussian::calculate_rayleigh_range(&wavelength, &e_radius),
-        ellipticity: 0.0,
-    };
-    sim.world
-        .create_entity()
-        .with(gaussian_beam)
-        .with(dipole::DipoleLight { wavelength })
-        .with(laser::frame::Frame {
-            x_vector: Vector3::x(),
             y_vector: Vector3::z(),
         })
         .build();
