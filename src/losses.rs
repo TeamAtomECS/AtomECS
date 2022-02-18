267487ca0280e13d31756872b18807af505a40fa
David Garrick <78102623+d-garrick@users.noreply.github.com>
2022-02-18 18:30:14 +0000

    Changed one body lifetime to one body loss rate for consistency with two body coefficient Added two body losses

--- src/losses.rs
+++ src/losses.rs
@@ -6,8 +6,10 @@ use crate::collisions::CollisionParameters;
 use crate::destructor::ToBeDestroyed;
 use crate::integrator::Timestep;
 use crate::partition::{DensityHashmap, PartitionCell, PartitionParameters};
+use rand::seq::index::sample;
 use rand::Rng;
 use specs::Entities;
+use specs::Entity;
 use specs::LazyUpdate;
 use specs::ParJoin;
 use specs::ReadStorage;
@@ -19,8 +21,10 @@ pub struct ApplyOneBodyLossOption;
 
 #[derive(Clone)]
 pub struct LossCoefficients {
-    pub one_body_lifetime: f64,
+    // Loss rate equal to 1 over one body lifetime (1/s)
+    pub one_body_loss_rate: f64,
 
+    // Two body loss rate coefficient (m^3/s)
     pub two_body_coefficient: f64,
 
     pub three_body_coefficient: f64,
@@ -29,7 +33,7 @@ pub struct LossCoefficients {
 impl Default for LossCoefficients {
     fn default() -> Self {
         LossCoefficients {
-            one_body_lifetime: 0.0,
+            one_body_loss_rate: 0.0,
             two_body_coefficient: 0.0,
             three_body_coefficient: 0.0,
         }
@@ -44,30 +48,34 @@ impl PartitionCell {
         collision_params: CollisionParameters,
         two_body_coefficient: f64,
         dt: f64,
-    ) {
+    ) -> Vec<Entity> {
         let atom_number = self.particle_number as f64 * collision_params.macroparticle;
         let density = atom_number / partition_params.box_width.powi(3);
 
         // two body loss rate: dN/dt = k_2 * n^2 * V = k_2 * n * N
         // So loss rate per particle is k_2 * n
-        let num_losses = two_body_coefficient * density * dt * self.particle_number as f64;
+        let mut num_losses = two_body_coefficient * density * dt * self.particle_number as f64;
 
-        let mut num_left = num_losses;
         let mut rng = rand::thread_rng();
-        while num_left > 0.0 {
-            if num_losses < 1.0 {
-                if rng.gen::<f64>() < num_losses {
-                    //TODO: delete the atom
-                    num_left = 0.0;
-                } else if num_losses > 1.0 {
-                    // TODO: delete atoms
-                }
-            } else {
-                // TODO
-                // num losses > atom_number?
-                // delete all
+        let mut entities_to_be_destroyed = Vec::new();
+
+        if num_losses > self.entities.len() as f64 {
+            num_losses = self.entities.len() as f64;
+        }
+        if num_losses < 1.0 {
+            if rng.gen::<f64>() < num_losses {
+                let idx = sample(&mut rng, self.entities.len(), 1).index(0);
+                entities_to_be_destroyed.push(self.entities[idx]);
+            }
+        } else if num_losses > 1.0 {
+            let idx_rand = sample(&mut rng, self.entities.len(), num_losses.round() as usize);
+
+            for idx in idx_rand {
+                entities_to_be_destroyed.push(self.entities[idx]);
             }
         }
+
+        entities_to_be_destroyed
     }
 }
 
@@ -81,11 +89,12 @@ impl<'a> System<'a> for ApplyTwoBodyLossSystem {
         ReadExpect<'a, PartitionParameters>,
         ReadExpect<'a, LossCoefficients>,
         WriteExpect<'a, DensityHashmap>,
+        Read<'a, LazyUpdate>,
     );
 
     fn run(
         &mut self,
-        (loss_option, t, collision_params, partition_params, losses, mut hashmap): Self::SystemData,
+        (loss_option, t, collision_params, partition_params, losses, mut hashmap,updater): Self::SystemData,
     ) {
         use rayon::prelude::*;
 
@@ -94,12 +103,16 @@ impl<'a> System<'a> for ApplyTwoBodyLossSystem {
             Some(_) => {
                 let cells: Vec<&mut PartitionCell> = hashmap.hashmap.values_mut().collect();
                 cells.into_par_iter().for_each(|partition_cell| {
-                    partition_cell.two_body_loss(
+                    let entities_to_be_destroyed = partition_cell.two_body_loss(
                         partition_params.clone(),
                         collision_params.clone(),
                         losses.two_body_coefficient,
                         t.delta,
                     );
+
+                    for e in entities_to_be_destroyed {
+                        updater.insert(e, ToBeDestroyed);
+                    }
                 });
             }
         }
@@ -125,7 +138,7 @@ impl<'a> System<'a> for ApplyOneBodyLossSystem {
             None => (),
             Some(_) => {
                 // probability of atom being lost in a small time dt << lifetime
-                let p_loss = t.delta / losses.one_body_lifetime;
+                let p_loss = t.delta * losses.one_body_loss_rate;
 
                 (&entities, &atoms).par_join().for_each(|(entity, _atom)| {
                     let mut rng = rand::thread_rng();
@@ -164,7 +177,7 @@ pub mod tests {
         test_world.insert(Timestep { delta: 1.0 });
         test_world.insert(ApplyOneBodyLossOption);
         test_world.insert(LossCoefficients {
-            one_body_lifetime: 1.0,
+            one_body_loss_rate: 1.0,
             two_body_coefficient: 0.0,
             three_body_coefficient: 0.0,
         });
